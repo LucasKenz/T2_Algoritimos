c28691f3e40eb58b21054b4a5773d0d0ec7e523b
LucasKenz <lkmtada@gmail.com>
2024-05-29 08:18:03 -0300

    tirando atributos da classe carro

--- carro/Carro.java
+++ carro/Carro.java
@@ -1,20 +1,15 @@
 package carro;
-import java.time.Duration;
-import java.time.LocalDateTime;
+
 
 public class Carro {
     private int anoFab;
     private String placa;
-    private LocalDateTime entrada;
-    private LocalDateTime saida;
-    private int manobras;
+
 
     public Carro(int anoFab, String placa) {
         this.anoFab = anoFab;
         this.placa = placa;
-        this.entrada = null;
-        this.saida = null;
-        this.manobras = 0;
+
     }
 
     public int getAnoFab() {
@@ -33,37 +28,8 @@ public class Carro {
         this.placa = placa;
     }
 
-    public LocalDateTime getEntrada() {
-        return entrada;
-    }
-
-    public void setEntrada(LocalDateTime entrada) {
-        this.entrada = entrada.withNano(entrada.getNano() / 1000000 * 1000000);
-    }
-
-    public LocalDateTime getSaida() {
-        return saida;
-    }
-
-    public void setSaida(LocalDateTime saida) {
-        this.saida = saida.withNano(saida.getNano() / 1000000 * 1000000);
-    }
-
-    public int getManobras() {
-        return manobras;
-    }
-
-    public void setManobras(int manobras) {
-        this.manobras = manobras;
-    }
-
-    public long getTempo() {
-        return Duration.between(entrada, saida).toSeconds();
-    }
-
     @Override
     public String toString() {
-        return "Carro [anoFab=" + anoFab + ", manobras=" + manobras + ", placa=" + placa
-                + ", entrada=" + entrada + ", saida=" + saida + "]";
+        return "Carro [anoFab=" + anoFab + ", placa=" + placa + "]";
     }
 }
